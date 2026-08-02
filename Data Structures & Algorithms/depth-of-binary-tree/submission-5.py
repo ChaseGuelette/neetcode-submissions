26d57fa3d6d5aa6bcae0b59e3cce6b1e72948ee3
ChaseGuelette <138884712+ChaseGuelette@users.noreply.github.com>
2026-08-02 15:43:16 -0400

    Add: depth-of-binary-tree - submission-5

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-5.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Recursive solution:
+        # if root is None:
+        #     return 0
+        
+        # left, right = 0,0
+        # if root.left is None and root.right is None:
+        #     return 1
+        # if root.left:
+        #     left = 1 + self.maxDepth(root.left)
+        # if root.right:
+        #     right =  1 + self.maxDepth(root.right)
+        # return max(right, left)
+
+        #BFS Solution 
+        # if root is None:
+        #     return 0
+        # q = deque()
+        # q.append((root, 1))
+        # node, level = None, 0
+        # while q:
+        #     node, level = q.popleft()
+        #     if node.right:
+        #         q.append((node.right, level + 1))
+        #     if node.left:
+        #         q.append((node.left, level + 1))
+        # return level
+
+        # NeetCode BFS solution, he uses a loop that process a whole level of the 
+        # Binary tree before moving on to the next level 
+        if root is None:
+            return 0
+        q = deque()
+        q.append(root)
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level 
+
+
+
+        
